ddc9a3eaa7e6f564e3ed5507b969295a1291dde4
aqin <1359637538@qq.com>
2020-05-04 10:30:25 +0800

    初版-git搭建

---
+++ stock/src/main/java/com/seeha/stock/StockApplication.java
@@ -0,0 +1,16 @@
+package com.seeha.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@MapperScan(basePackages = "com.seeha.stock.**.mapper")
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+public class StockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockApplication.class, args);
+	}
+
+}
